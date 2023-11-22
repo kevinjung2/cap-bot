dc2f0fd3d704b96ed0c2c72faef1b93ecf6b0d9e
Kevin Jung <kev.jung.ii@gmail.com>
2023-11-21 20:47:58 -0500

    updates the hello message with instructions on how to use cap bot

--- commands/hello.js
+++ commands/hello.js
@@ -1,9 +1,13 @@
-import { SlashCommandBuilder } from 'discord.js'
+import { SlashCommandBuilder } from 'discord.js';
+
+const reply = `Hi! I am Cap Bot and I'm here to let you know who's been capping in this server. 
+			   Use /cap to let everyone know whos been capping and what they're yappin about now.
+			   Use /leaderboard to see the biggest caps, or cappers in the server.`;
 
 export const data = new SlashCommandBuilder()
-  .setName('hello')
-  .setDescription('Says hello')
+	.setName('hello')
+	.setDescription('Introduces cap bot and how to use it.');
 
-export async function execute(interaction){
-  await interaction.reply('Hello!')
+export async function execute(interaction) {
+	await interaction.reply(reply);
 }
